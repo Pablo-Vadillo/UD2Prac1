5f9a8a89cb2cf7deb4f5d80229f9de9f3ee20c8e
= <=>
2023-11-15 16:23:51 +0000

    feat:Experimento 1 Done IMAGE ADDED.

--- src/net/salesianos/experimentos/ExperimentoUno.java
+++ src/net/salesianos/experimentos/ExperimentoUno.java
@@ -1,4 +1,18 @@
 package net.salesianos.experimentos;
 
+import net.salesianos.granjeroapp.Cliente;
+import net.salesianos.granjeroapp.Granjero;
+import net.salesianos.granjeroapp.Restaurante;
+
 public class ExperimentoUno {
+    public static void main(String[] args) {
+
+        Restaurante restaurante = new Restaurante(20);
+
+        Thread granjero1 = new Thread(new Granjero("Granjero1", 5, restaurante));
+        Thread cliente1 = new Thread(new Cliente("Cliente1", 5, restaurante));
+
+        granjero1.start();
+        cliente1.start();
+    }
 }
